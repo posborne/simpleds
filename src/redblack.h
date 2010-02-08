4ac1451359f600b4d474df60526155987a0e247a
Paul Osborne <paul-osborne@bethel.edu>
2010-02-07 22:54:53 -0600

    Added some initial stubs and header design for redblack tree

---
+++ src/redblack.h
@@ -0,0 +1,67 @@
+/* 
+ * redblack.h
+ * 
+ * Copyright (c) 2010 Paul Osborne <osbpau@gmail.com>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#ifndef REDBLACK_HEADER_H
+#define REDBLACK_HEADER_H
+
+#include <stdint.h>
+
+enum redblack_iteration_order {
+	REDBLACK_ITERATE_PREORDER,
+	REDBLACK_ITERATE_INORDER,
+	REDBLACK_ITERATE_POSTORDER
+};
+
+typedef struct redblack_node {
+	uint8_t red; /* 0 if black, 1 if red */
+	struct redblack_node *left;
+	struct redblack_node *right;
+	struct redblack_node *parent;
+} *RedblackNode;
+
+typedef struct redblack_iterator {
+	enum redblack_iteration_order iteration_order;
+	RedblackNode currentNode;
+	
+} *RedblackIterator;
+
+typedef struct redblack_tree {
+	RedblackNode root;
+	uint8_t(*compare_func)(const void*, const void*);
+} *RedblackTree;
+
+/* function prototypes */
+RedblackTree redblack_create(uint8_t(*compare_func)(const void*, const void*));
+void         redblack_insert(RedblackTree tree, void* item);
+void*        redblack_remove(RedblackTree tree, void* item);
+int8_t       redblack_contains(RedblackTree tree, void* item);
+
+/* iterator functions */
+RedblackIterator redblack_iterator_create(RedblackTree tree, 
+										  uint8_t iteration_order);
+void             redblack_iterator_free(RedblackIterator iterator);
+RedblackNode     redblack_iterator_next(RedblackIterator iterator);
+int8_t 	         redblack_iterator_hasnext(RedblackIterator iterator);
+
+#endif
